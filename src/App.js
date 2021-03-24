027afc6869dd32ad44db8686f4f0fdfc86635fba
Jinwook-Song <wlsdnr129@naver.com>
2021-03-24 18:57:31 +0900

    lecture 2.2 Dynamic Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 // import React from "react";
 
-function Food({ fav }) {
-  return <h2>like {fav}</h2>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
+const foodILike = [
+  {
+    name: "cake",
+    image:
+      "https://cupfulofkale.com/wp-content/uploads/2019/09/Vegan-Blackberry-and-Gin-Cake-Cut-Open.jpeg",
+  },
+  {
+    name: "kimbap",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.BvdzjW36CWQYyayXvduatAHaHR%26pid%3DApi&f=1",
+  },
+];
 
 function App() {
   return (
     <div>
       <h1>Hanyang</h1>
-      <Food fav="milk" />
-      <Food fav="cake" />
-      <Food fav="ramen" />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
