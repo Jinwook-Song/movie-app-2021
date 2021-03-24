48ee7a1164b725977eb638afd4b87e196b6499cf
Jinwook-Song <wlsdnr129@naver.com>
2021-03-24 18:37:06 +0900

    lecture 2.1 Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 // import React from "react";
 
+function Food({ fav }) {
+  return <h2>like {fav}</h2>;
+}
+
 function App() {
   return (
     <div>
-      <h1>Hi~!</h1>
+      <h1>Hanyang</h1>
+      <Food fav="milk" />
+      <Food fav="cake" />
+      <Food fav="ramen" />
     </div>
   );
 }
