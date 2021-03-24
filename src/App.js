716634b109f105b646ad46f385f5278adf1d0dd4
Jinwook-Song <wlsdnr129@naver.com>
2021-03-24 19:16:57 +0900

    lectur2.3 map

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
 // import React from "react";
 
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+  },
+  {
+    id: 3,
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+  },
+  {
+    id: 4,
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+  },
+  {
+    id: 5,
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+  },
+];
+
 function Food({ name, picture }) {
   return (
     <div>
       <h2>I like {name}</h2>
-      <img src={picture} />
+      <img src={picture} alt={name} />
     </div>
   );
 }
-const foodILike = [
-  {
-    name: "cake",
-    image:
-      "https://cupfulofkale.com/wp-content/uploads/2019/09/Vegan-Blackberry-and-Gin-Cake-Cut-Open.jpeg",
-  },
-  {
-    name: "kimbap",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.BvdzjW36CWQYyayXvduatAHaHR%26pid%3DApi&f=1",
-  },
-];
 
 function App() {
   return (
     <div>
-      <h1>Hanyang</h1>
       {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
