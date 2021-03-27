9ef8e84520c5eb9a9060c478ed1159dc5f185da2
Jinwook-Song <wlsdnr129@naver.com>
2021-03-27 18:46:58 +0900

    lecture 4.4 Styles

--- src/Movie.js
+++ src/Movie.js
@@ -4,12 +4,12 @@ import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movies__movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
-        <h3 className="movie__year">{year}</h3>
-        <ul className="genres">
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
             <li key={index} className="genres__genre">
               {genre}
